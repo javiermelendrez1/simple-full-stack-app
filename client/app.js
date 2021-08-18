9339df3d877bdcdcc122942002dce473625a8024
Javier Melendrez <81501034+javiermelendrez1@users.noreply.github.com>
2021-08-18 12:30:49 -0700

    added event listener
    - listening for domcontentloaded

--- client/app.js
+++ client/app.js
@@ -1,1 +1,12 @@
 console.log('app.js is connected');
+
+document.addEventListener('DOMContentLoaded', () => {
+    loadHTMLTable([]);
+});
+
+const loadHTMLTable = (data) => {
+    const table = document.querySelector('table tbody');
+    if(data.length === 0) {
+        table.innerHTML = "<tr><td class='no-data' colspan='5'>No Data</td></tr>";
+    }
+}
